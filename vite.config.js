30dbaeb95d72220f67a90e04d43a385655946867
Lucas Marques <lucas.marques@yolocoliving.com.br>
2025-11-16 15:50:58 -0300

    Tentenado nova config vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    strictPort: true,
+    port: 3000,
     allowedHosts: [
-      "e1800c2a-d1b7-41fc-ad0c-4b9905f207e1-00-2qf40b6f6hyqy.spock.replit.dev"
-    ]
+      ".replit.dev",
+      ".spock.replit.dev"
+    ],
+    hmr: {
+      host: "localhost",
+    }
   }
 })
