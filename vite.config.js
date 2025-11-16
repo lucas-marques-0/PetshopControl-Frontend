f3ffa42ab308e5ee48ad62734c76dfde747203b5
Lucas Marques <lucas.marques@yolocoliving.com.br>
2025-11-16 15:46:19 -0300

    Mudando configuração vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      "e1800c2a-d1b7-41fc-ad0c-4b9905f207e1-00-2qf40b6f6hyqy.spock.replit.dev"
+    ]
+  }
 })
